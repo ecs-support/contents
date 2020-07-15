73c4b822da0bf58d481b1683a686b7f8972eb88f
yosarawut <yo_sarawut@hotmail.com>
2020-07-16 02:18:09 +0700

    .

---
+++ reference/static/js/script.js
@@ -0,0 +1,10 @@
+function newDoc() {
+     
+    document.location = "https://ecs-support.github.io/KM/customs/";
+}
+
+function displayPage(pages) {
+
+    document.getElementById('header-img').style.display = "none";
+    document.getElementById('show_pages').src = (pages);
+}
