79e3aabe62a57771ea3a99c0e03769af49aa81e5
yo-sarawut <yo_sarawut@hotmail.com>
2021-07-25 13:45:48 +0700

    .

--- Lab/app/GC/login/login.js
+++ Lab/app/GC/login/login.js
@@ -1,4 +1,4 @@
-var attempt = 3; // Variable to count number of attempts.
+var attempt = 0; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate() {
   //var username = document.getElementById("username").value;
