5955d726524f16efb31ad8aa16996cbdc7656470
yo-sarawut <yo_sarawut@hotmail.com>
2022-08-29 02:21:17 +0700

    .

---
+++ Lab/Kids/homework/reading_aloud/player/js/music-list.js
@@ -0,0 +1,81 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Text 1",
+    artist: "David",
+    img: "david",
+    src: "David_text_01",
+  },
+  {
+    name: "Text 2",
+    artist: "David",
+    img: "david1",
+    src: "David_text_02",
+  },
+  {
+    name: "Text 3",
+    artist: "David",
+    img: "david",
+    src: "David_text_03",
+  },
+  {
+    name: "Text 4",
+    artist: "David1",
+    img: "david",
+    src: "David_text_04",
+  },
+  {
+    name: "Text 5",
+    artist: "David",
+    img: "david",
+    src: "David_text_05",
+  },
+  {
+    name: "Text 1",
+    artist: "Zira",
+    img: "zira",
+    src: "Zira_text_01",
+  },
+  {
+    name: "Text 2",
+    artist: "Zira",
+    img: "zira1",
+    src: "Zira_text_02",
+  },
+  {
+    name: "Text 3",
+    artist: "Zira",
+    img: "zira",
+    src: "Zira_text_03",
+  },
+  {
+    name: "Text 4",
+    artist: "Zira",
+    img: "zira1",
+    src: "Zira_text_04",
+  },
+  {
+    name: "Text 5",
+    artist: "Zira",
+    img: "zira",
+    src: "Zira_text_05",
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
